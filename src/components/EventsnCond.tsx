684d97b9fcd657d0f9c6e257cf3da25647748ee0
SidW111 <siddhantwainngade@gmail.com>
2025-09-10 13:43:27 +0530

    Event handling and conditional rendering

--- src/components/EventsnCond.tsx
+++ src/components/EventsnCond.tsx
@@ -1,11 +1,16 @@
 import Code from "./ReUsable/Code";
+import Highlight from "./ReUsable/HighLight";
 import Wrapper from "./ReUsable/Wrapper";
 
 export default function HandlingEventsnConditional() {
   return (
     <Wrapper title="Handling Events & Conditional Rendering">
+      <p className="text-2xl font-bold">Handling Events</p>
       <p>React handles almost everything like HTML, but with few changes</p>
-      <p>Events are written in camelCase ( onClick, onChange ).</p>
+      <p>
+        Events are written in camelCase ( <Highlight>onClick</Highlight>,
+        <Highlight>onChange</Highlight> ).
+      </p>
       <p>Instead of Strings, you pass a function.</p>
       <p className="font-bold">Example</p>
       <Code>
@@ -15,9 +20,46 @@ export default function HandlingEventsnConditional() {
         <p>{"}"}</p>
 
         <p>
-          return &lt;button onClick= {"{"} handleClick {"}"} &gt; Click Me
-          &lt;/button&gt;
+          return &lt;button{" "}
+          <span className="bg-green-600 text-center rounded-md  px-0.5 py-0.5 text-white">
+            onClick= {"{"} handleClick {"}"}
+          </span>{" "}
+          &gt; Click Me &lt;/button&gt;
         </p>
+        <p>{"}"}</p>
+      </Code>
+      <p className="font-bold">In HTML</p>
+      <Code>
+        &lt;button{"  "}
+        <span className="bg-green-600 text-center rounded-md  px-0.5 py-0.5 text-white">
+          onClick="handleClick()"
+        </span>{" "}
+        &gt; Click Me &lt; /button&gt;;
+      </Code>
+      <p>
+        React Uses JS <span className="font-bold">functions</span> not strings
+      </p>
+
+      <p className="text-2xl font-bold mt-2 mb-1">Conditional Rendering</p>
+      <p className="">
+        React lets you render different UI based on conditions (like if or
+        ternary operator).{" "}
+      </p>
+      <p className="font-bold">Example:- Login Button</p>
+      <p>✅ If isLoggedIn = true → shows Logout.</p>
+      <p>✅ If isLoggedIn = false → shows Login.</p>
+      <Code>
+        <p>
+          function LoginButton({"{"} isLoggedIn {"}"}) {"{"}
+        </p>
+        <p>return (</p>
+        <p> &lt;div&gt; </p>
+        <p>
+          {"{"} isLoggedIn ? &lt;button&gt; Logout &lt; /button&gt; :
+          &lt;button&gt; Login &lt; /button&gt; {"}"}
+        </p>
+        <p> &lt;/div&gt; </p>
+        <p>);</p>
         <p>{"}"}</p>
       </Code>
     </Wrapper>
--- src/components/ReUsable/HighLight.tsx
+++ src/components/ReUsable/HighLight.tsx
@@ -1,7 +1,5 @@
 import type { ReactNode } from "react";
 
-export default function Highlight({children}:{children:ReactNode}){
-return <span className="bg-gray-200 px-2 py-1 rounded-md">
-    {children}
-</span>
+export default function Highlight({ children }: { children: ReactNode }) {
+  return <span className="bg-gray-200 px-2 py-0.5 rounded-md">{children}</span>;
 }
