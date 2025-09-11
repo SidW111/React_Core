c12dbd268d1550dc0f3d664d670b1afe5a3cb4d1
SidW111 <siddhantwainngade@gmail.com>
2025-09-11 13:28:20 +0530

    useRef

---
+++ src/components/UseRef.tsx
@@ -0,0 +1,8 @@
+import Highlight from "./ReUsable/HighLight";
+import Wrapper from "./ReUsable/Wrapper";
+
+export default function UseRef(){
+    return <Wrapper title="UseRef">
+        <p> <Highlight>useRef</Highlight> </p>
+    </Wrapper>
+}
