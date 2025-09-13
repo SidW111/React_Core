482c341aa65f0b2a7afc0e1c30048edd4ab8943f
SidW111 <siddhantwainngade@gmail.com>
2025-09-13 16:03:40 +0530

    APi Integration

---
+++ src/components/IntegratingAPIs.tsx
@@ -0,0 +1,35 @@
+import Code from "./ReUsable/Code";
+import Wrapper from "./ReUsable/Wrapper";
+
+export default function IntegratingAPI() {
+  return (
+    <Wrapper title="Integrating APIs (REST, GraphQL)">
+      <p className="font-bold text-xl">REST API</p>
+      <p>Standard HTTP methods: GET, POST, PUT, DELETE.</p>
+      <p>Fetching example using Axios:</p>
+      <Code>
+        <pre>
+          {`import axios from "axios"
+    const fetchUsers = async() => {
+        const response = await axios.get("https://wp.com");
+        console.log(response.data);
+    };`}
+        </pre>
+      </Code>
+      <p className="font-bold text-xl">GraphQL</p>
+      <p>Single endpoint, flexible queries.</p>
+      <p>Advantages: fetch exactly what you need.</p>
+      <Code>
+        <pre>
+         {`query {
+  user(id: "1") {
+    name
+    email
+  }
+}
+`}
+        </pre>
+      </Code>
+    </Wrapper>
+  );
+}
