6b0e1886c813ebc1fa107a8ed1beb860f263c09e
SidW111 <siddhantwainngade@gmail.com>
2025-09-16 13:33:02 +0530

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import MyRouter from "./components/MyRouter";
 
+
 function App() {
   return (
     <BrowserRouter>
       <MyRouter />
+
     </BrowserRouter>
   );
 }
