f705f28942c030e8a9526f2112a611fcdb970ade
SidW111 <siddhantwainngade@gmail.com>
2025-09-16 12:11:39 +0530

    Logo

--- src/components/ReUsable/Wrapper.tsx
+++ src/components/ReUsable/Wrapper.tsx
@@ -9,7 +9,7 @@ export default function Wrapper({
 }) {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center ">
-      <h1 className="text-3xl font-bold text-center mb-1">{title}</h1>
+      <h1 className="text-3xl font-bold text-center mt-4 mb-4">{title}</h1>
       <div className="font-semibold tracking-tight">
         <div className="p-2 flex flex-col gap-1">{children}</div>
       </div>
