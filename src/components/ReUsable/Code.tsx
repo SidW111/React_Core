cbffa6ec656202f1262e170cfb208ef8ecf98e0f
SidW111 <siddhantwainngade@gmail.com>
2025-09-16 12:24:58 +0530

    Responsive

--- src/components/ReUsable/Code.tsx
+++ src/components/ReUsable/Code.tsx
@@ -2,9 +2,9 @@ import type { ReactNode } from "react";
 
 export default function Code({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-gray-900 text-white p-3 rounded-xl mt-2 mb-2">
-      <pre className="whitespace-pre-wrap break-words text-sm md:text-base">
-      {children}
+    <div className="bg-gray-900 text-white p-3 rounded-xl mt-2 mb-2 w-full max-w-full">
+      <pre className="whitespace-pre-wrap break-all text-xs sm:text-sm md:text-base max-w-full">
+        {children}
       </pre>
     </div>
   );
--- src/components/ReUsable/Wrapper.tsx
+++ src/components/ReUsable/Wrapper.tsx
@@ -8,8 +8,8 @@ export default function Wrapper({
   children: ReactNode;
 }) {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center ">
-      <h1 className="text-3xl font-bold text-center mt-4 mb-4">{title}</h1>
+    <div className="w-full h-full flex flex-col justify-center items-center mb-10">
+      <h1 className="text-3xl font-bold text-center mt-4 mb-1">{title}</h1>
       <div className="font-semibold tracking-tight">
         <div className="p-2 flex flex-col gap-1">{children}</div>
       </div>
