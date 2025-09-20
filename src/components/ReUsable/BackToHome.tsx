711391faf783dd22525eb6e6bfe58037128e971e
SidW111 <siddhantwainngade@gmail.com>
2025-09-20 22:42:20 +0530

    haha

--- src/components/ReUsable/BackToHome.tsx
+++ src/components/ReUsable/BackToHome.tsx
@@ -1,3 +1,4 @@
+import { FaLongArrowAltLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function BackToHome() {
@@ -5,10 +6,10 @@ export default function BackToHome() {
     <div className="flex justify-end items-center mt-4 w-full">
       <Link
         to="/"
-        className="inline-block px-4 py-2 rounded-xl bg-blue-700 text-white text-center text-sm font-medium shadow-md 
+        className="flex items-center justify-center gap-1 px-4 py-2 rounded-xl bg-blue-700 text-white  text-sm font-medium shadow-md 
                    hover:bg-blue-800 active:scale-95 transition-transform duration-150"
       >
-        ⬅ Back to Home
+        <FaLongArrowAltLeft /> Back to Home
       </Link>
     </div>
   );
