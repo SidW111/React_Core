28aa1d92b006c52a3f7ee0f1fb5e0b547efc2922
SidW111 <siddhantwainngade@gmail.com>
2025-09-20 22:34:47 +0530

    final

--- src/components/ReUsable/BackToHome.tsx
+++ src/components/ReUsable/BackToHome.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function BackToHome() {
   return (
-    <div className="flex justify-end mt-4 w-full">
+    <div className="flex justify-end items-center mt-4 w-full">
       <Link
         to="/"
-        className="inline-block px-4 py-2 rounded-xl bg-blue-700 text-white text-sm font-medium shadow-md 
+        className="inline-block px-4 py-2 rounded-xl bg-blue-700 text-white text-center text-sm font-medium shadow-md 
                    hover:bg-blue-800 active:scale-95 transition-transform duration-150"
       >
         ⬅ Back to Home
