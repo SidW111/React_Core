900d15399dee3e948f1f8bf10e46cab917208f0f
SidW111 <siddhantwainngade@gmail.com>
2025-09-20 22:44:37 +0530

    final

--- src/components/ReUsable/BackToHome.tsx
+++ src/components/ReUsable/BackToHome.tsx
@@ -6,7 +6,7 @@ export default function BackToHome() {
     <div className="flex justify-end items-center mt-4 w-full">
       <Link
         to="/"
-        className="flex items-center justify-center gap-1 px-4 py-2 rounded-xl bg-blue-700 text-white  text-sm font-medium shadow-md 
+        className="flex items-center justify-center gap-2 px-3 py-2 rounded-lg bg-blue-700 text-white text-sm font-medium shadow-md 
                    hover:bg-blue-800 active:scale-95 transition-transform duration-150"
       >
         <FaLongArrowAltLeft /> Back to Home
